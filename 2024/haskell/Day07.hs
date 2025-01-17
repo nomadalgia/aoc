35d18eb00039c7eade2f0b23a11be70b77799405
Emilia P. Stoyanova <emiliaps31@gmail.com>
2025-01-17 16:03:45 +0100

    Solve 2024/**/Day07.hs

---
+++ 2024/haskell/Day07.hs
@@ -0,0 +1,39 @@
+module Day07 where
+
+import Data.List.Extra
+import Data.Tuple.Extra
+import Data.Maybe
+import Data.Composition
+import Data.Bifunctor
+import Data.Function
+
+type Op = Int -> Int -> Int
+
+main :: IO ()
+main = interact (show . solve . parse)
+
+parse :: String -> [(Int, [Int])]
+parse = mapMaybe parseLine . lines
+  where parseLine = fmap (bimap read (map read . words)) . stripInfix ":"
+
+solve :: [(Int, [Int])] -> (Int, Int)
+solve = p1 &&& p2
+
+p1, p2 :: [(Int, [Int])] -> Int
+p1 = common [(+), (*)]
+p2 = common [(+), (*), flip comb]
+
+common :: [Op] -> [(Int, [Int])] -> Int
+common ops = sumOn' fst . filter (\(r, xs) -> r `elem` gen xs ops)
+
+-------------
+--- Utils ---
+-------------
+gen :: [Int] -> [Op] -> [Int]
+gen xs ops = go $ reverse xs where
+  go []     = []
+  go [x]    = [x]
+  go (x:xs) = [op x y | y <- go xs, op <- ops]
+
+comb :: Int -> Int -> Int
+comb = read .: (++) `on` show
