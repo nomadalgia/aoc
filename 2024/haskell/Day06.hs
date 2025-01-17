caf336f1b80ab4f0c0ef82db03b7a393f633ee12
Emilia P. Stoyanova <emiliaps31@gmail.com>
2025-01-17 16:03:05 +0100

    Solve 2024/**/Day06.hs (Part 2)
    Please read the top comment before passing judgmenet onto me.

--- 2024/haskell/Day06.hs
+++ 2024/haskell/Day06.hs
@@ -6,6 +6,11 @@ import Data.Maybe
 import Control.Applicative
 import Data.Composition
 
+{-
+ - By far the ugliest piece of Haskell code ever written by me. T_T
+ - Please let me be forgiven. I want not to refactor; I want to leave.
+ -}
+
 type Coords = (Int, Int) -- (x, y)
 data Dir = U | D | R | L deriving (Eq, Show)
 
@@ -19,8 +24,9 @@ solve :: ([Coords], Int, (Dir, Coords)) -> (Int, Int)
 solve = p1 &&& p2
 
 p1, p2 :: ([Coords], Int, (Dir, Coords)) -> Int
-p1 = length . nubOrd . (uncurry3 $ unfoldr .: move)
-p2 = const maxBound
+p1 = length . nubOrd . uncurry3 (unfoldr .: move)
+p2 (obstacles, border, guard) = length $ filter id $ map (\coord -> isCycle (coord : obstacles) border guard) path
+  where path = nubOrd $ unfoldr (move obstacles border) guard
 
 -------------
 --- Utils ---
@@ -28,7 +34,7 @@ p2 = const maxBound
 parseObstacles :: [String] -> [Coords]
 parseObstacles = concat . zipWithFrom (map . flip (,)) 0 . map (elemIndices '#')
 
--- If we call elemIndex '^' on the origina unlined list, we can then `div` and
+-- If we call elemIndex '^' on the original unlined list, we can then `div` and
 -- `mod` it by the line length to get the coordinate values more easily.
 parseGuard :: [String] -> (Dir, Coords)
 parseGuard = (U,) . head . catMaybes . zipWith (liftA2 $ flip (,)) (map Just [0..]) . map (elemIndex '^')
@@ -54,3 +60,12 @@ move obstacles _ (L, c) = let moved = first pred c in
                             if   moved `elem` obstacles
                             then Just (c, (U, c)    )
                             else Just (c, (L, moved))
+
+isCycle :: [Coords] -> Int -> (Dir, Coords) -> Bool
+isCycle obstacles border guard = go obstacles border guard [] where
+  go obstacles border guard@(dir, (x,y)) visited | x < 0 || y < 0 || x > border || y > border = False
+  go obstacles border guard@(dir, (x,y)) visited | guard `elem` visited = True
+  go obstacles border guard@(U, c@(x,y)) visited = let moved = second pred c in go obstacles border (if moved `elem` obstacles then (R, c) else (U, moved)) (guard : visited)
+  go obstacles border guard@(D, c@(x,y)) visited = let moved = second succ c in go obstacles border (if moved `elem` obstacles then (L, c) else (D, moved)) (guard : visited)
+  go obstacles border guard@(R, c@(x,y)) visited = let moved = first  succ c in go obstacles border (if moved `elem` obstacles then (D, c) else (R, moved)) (guard : visited)
+  go obstacles border guard@(L, c@(x,y)) visited = let moved = first  pred c in go obstacles border (if moved `elem` obstacles then (U, c) else (L, moved)) (guard : visited)
