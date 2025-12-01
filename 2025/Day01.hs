5a78fb45091d76d236fc71b8bd5b3d857b183658
Emilia P. Stoyanova <emiliaps31@gmail.com>
2025-12-01 14:31:12 +0100

    feat: 2025/Day01/p2

--- 2025/Day01.hs
+++ 2025/Day01.hs
@@ -10,12 +10,13 @@ import qualified Data.Map       as M
 main :: IO ()
 main = T.interact (show . solve . parse)
 
+solve :: [Int] -> (Int, Int)
 solve = p1 &&& p2
 
-p1 :: [Int] -> Int
-p1 = length . filter (0==) . scanl wrapDial 50
-  where wrapDial a b = (a + b) `mod` 100
-p2 = id
+p1, p2, common :: [Int] -> Int
+p1 = common
+p2 = common . concatMap unitList
+common = length . filter (0==) . scanl (wrapAdd 100) 50
 
 parse :: Text -> [Int]
 parse = map fst . rights . map (T.signed T.decimal) . T.lines . replaceMany [('R', '+'), ('L', '-')]
@@ -23,5 +24,12 @@ parse = map fst . rights . map (T.signed T.decimal) . T.lines . replaceMany [('R
 -------------
 --- Utils ---
 -------------
+wrapAdd :: Int -> Int -> Int -> Int
+wrapAdd modulus a b = (a + b) `mod` modulus
+
 replaceMany :: [(Char, Char)] -> Text -> Text
 replaceMany rules = T.map (\c -> M.findWithDefault c c (M.fromList rules))
+
+unitList :: Int -> [Int]
+unitList n | n == 0    = []
+           | otherwise = replicate (abs n) (signum n)
