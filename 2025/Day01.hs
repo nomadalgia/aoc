82d6ab1c0fb4e1f0f1f95a47cdd3a334c68a29ae
Emilia P. Stoyanova <emiliaps31@gmail.com>
2025-12-01 17:15:23 +0100

    perf: 2025 Day01 combine p1 and p2 calculations

--- 2025/Day01.hs
+++ 2025/Day01.hs
@@ -2,7 +2,6 @@
 module Day01 where
 
 import Relude
-import Data.Composition
 import qualified Data.Text      as T
 import qualified Data.Text.IO   as T
 import qualified Data.Text.Read as T
@@ -10,17 +9,19 @@ import qualified Data.Text.Read as T
 main :: IO ()
 main = T.interact (show . solve . parse)
 
-solve :: [Int] -> (Int, Int)
-solve = p1 &&& p2
+solve :: [Integer] -> (Integer, Integer)
+solve = (p1 &&& p2) . common
 
-p1, p2 :: [Int] -> Int
-p1 = length . filter (==0) . scanl (flip mod 100 .: (+)) 50
-p2 = fst . foldl' (\(n,a) b -> first ((n+) . abs) $ (a + b) `divMod` 100) (0,50)
+p1, p2 :: [(Integer, Integer)] -> Integer
+p1 = genericLength . filter (==0) . map snd
+p2 = maybe 0 fst . viaNonEmpty last
 
-parse :: Text -> [Int]
-parse = map fst
-      . rights
-      . map (T.signed T.decimal)
+common :: [Integer] -> [(Integer, Integer)]
+common = scanl (\(n,a) b -> first ((n+) . abs) $ (a + b) `divMod` 100) (0,50)
+
+parse :: Text -> [Integer]
+parse = rights
+      . map (fmap fst . T.signed T.decimal)
       . T.lines
       . T.map (\case 'R' -> '+';
                      'L' -> '-';
