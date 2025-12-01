922de3547e9803bf6c24b050890aeda683861ede
Emilia P. Stoyanova <emiliaps31@gmail.com>
2025-12-01 15:02:25 +0100

    perf: 2025 Day01

--- 2025/Day01.hs
+++ 2025/Day01.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoImplicitPrelude, LambdaCase #-}
 module Day01 where
 
 import Relude
 import qualified Data.Text      as T
 import qualified Data.Text.IO   as T
 import qualified Data.Text.Read as T
-import qualified Data.Map       as M
 
 main :: IO ()
 main = T.interact (show . solve . parse)
@@ -16,20 +15,22 @@ solve = p1 &&& p2
 p1, p2, common :: [Int] -> Int
 p1 = common
 p2 = common . concatMap unitList
-common = length . filter (0==) . scanl (wrapAdd 100) 50
+common = length . filter (==0) . scanl (wrapAdd 100) 50
 
 parse :: Text -> [Int]
-parse = map fst . rights . map (T.signed T.decimal) . T.lines . replaceMany [('R', '+'), ('L', '-')]
+parse = map fst
+      . rights
+      . map (T.signed T.decimal)
+      . T.lines
+      . T.map (\case 'R' -> '+';
+                     'L' -> '-';
+                      c   -> c )
 
 -------------
 --- Utils ---
 -------------
 wrapAdd :: Int -> Int -> Int -> Int
-wrapAdd modulus a b = (a + b) `mod` modulus
-
-replaceMany :: [(Char, Char)] -> Text -> Text
-replaceMany rules = T.map (\c -> M.findWithDefault c c (M.fromList rules))
+wrapAdd m a b = (a + b) `mod` m
 
 unitList :: Int -> [Int]
-unitList n | n == 0    = []
-           | otherwise = replicate (abs n) (signum n)
+unitList n = replicate (abs n) (signum n)
